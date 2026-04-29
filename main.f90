0ed560a7add10a12b6312b5cd24f837ad900aaf4
Louis Nuttall <tzm513@york.ac.uk>
2026-04-29 13:02:58 +0100

    Created magnet type to store data about magnetic objects Size, charge across a point, and position are all currently stored

--- main.f90
+++ main.f90
@@ -1,4 +1,19 @@
 program magnetostatics
     use consts
+    implicit none
+
+        ! Create variable type to store details of the bar magnet inducing the force
+    type :: magnet(xlen, ylen, zlen)
+        integer, len :: xlen, ylen, zlen
+
+            ! Charge difference over each position
+        type(vector), dimension(xlen, ylen, zlen)   :: charges
+            ! Centre coordinate of the magnet
+        type(vector)                :: centre(3)
+            ! Distance between each point of the magnet
+        real(kind = dp)             :: dx
+
+    end type
     
+    type(magnet(2, 2, 10)) :: bar
 end program
