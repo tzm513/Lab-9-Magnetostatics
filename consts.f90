ab0cd9a023023d2b31b074f697327f87cb52a665
tzm513 <tzm513@york.ac.uk>
2026-04-30 13:54:01 +0100

    Increased compatability of mathematics between vector type variables and numerical variables

--- consts.f90
+++ consts.f90
@@ -3,30 +3,100 @@ module consts
 
     integer, parameter  :: dp = selected_real_kind(15, 300)
 
-    real(kind = dp), parameter  :: pi = 3.1415926
+    real(kind = dp), parameter      :: pi = 3.1415926
     complex(kind = dp), parameter   :: i = cmplx(0.0_dp, 1.0_dp, kind=dp)
+        ! Permeability of free space
+    real(kind = dp), parameter      :: mu0 = 4*pi*(10.0**(-7))
 
     type :: vector
         real(kind = dp) :: i
         real(kind = dp) :: j
         real(kind = dp) :: k
     end type
+    
+    interface operator(+)
+        procedure vec_add
+        procedure vec_add_int_1
+        procedure vec_add_real_1
+        procedure vec_add_int_2
+        procedure vec_add_real_2
+    end interface
 
     interface operator(-)
         procedure vec_subt
     end interface
-    
-    interface operator(+)
-        procedure vec_add
+
+    interface operator(*)
+        procedure vec_mult_int_1
+        procedure vec_mult_real_1
+        procedure vec_mult_int_2
+        procedure vec_mult_real_2
     end interface
 
-    interface vec_div
+    interface operator(/)
         procedure vec_div_int
         procedure vec_div_real
     end interface
 
     contains
+
+        pure function vec_add(a, b) result(c)
+            type(vector), intent(in)    :: a, b
+            type(vector)                :: c
+            
+            c%i = a%i + b%i
+            c%j = a%j + b%j
+            c%k = a%k + b%k
+        end function
+
+        pure function vec_add_int_1(a, b) result(c)
+            type(vector), intent(in)    :: a
+            integer, intent(in)         :: b
+            type(vector)                :: c
+
+            real(kind = dp)             :: b_real
+
+            b_real = real(b, kind = dp)
+            
+            c%i = a%i + b_real
+            c%j = a%j + b_real
+            c%k = a%k + b_real
+        end function
     
+        pure function vec_add_real_1(a, b) result(c)
+            type(vector), intent(in)    :: a
+            real(kind = dp), intent(in) :: b
+            type(vector)                :: c
+            
+            c%i = a%i + b
+            c%j = a%j + b
+            c%k = a%k + b
+        end function
+
+        pure function vec_add_int_2(b, a) result(c)
+            type(vector), intent(in)    :: a
+            integer, intent(in)         :: b
+            type(vector)                :: c
+
+            real(kind = dp)             :: b_real
+
+            b_real = real(b, kind = dp)
+            
+            c%i = a%i + b_real
+            c%j = a%j + b_real
+            c%k = a%k + b_real
+        end function
+    
+        pure function vec_add_real_2(b, a) result(c)
+            type(vector), intent(in)    :: a
+            real(kind = dp), intent(in) :: b
+            type(vector)                :: c
+            
+            c%i = a%i + b
+            c%j = a%j + b
+            c%k = a%k + b
+        end function
+
         pure function vec_subt(a, b) result(c)
             type(vector), intent(in)    :: a, b
             type(vector)                :: c
@@ -36,13 +106,52 @@ module consts
             c%k = a%k - b%k
         end function
 
-        pure function vec_add(a, b) result(c)
-            type(vector), intent(in)    :: a, b
+        pure function vec_mult_int_1(a, b) result(c)
+            type(vector), intent(in)    :: a
+            integer, intent(in)         :: b
             type(vector)                :: c
-            
-            c%i = a%i + b%i
-            c%j = a%j + b%j
-            c%k = a%k + b%k
+
+            real(kind = dp)             :: b_real
+
+            b_real = real(b, kind = dp)
+
+            c%i = a%i * b_real
+            c%j = a%j * b_real
+            c%k = a%k * b_real
+        end function
+
+        pure function vec_mult_real_1(a, b) result(c)
+            type(vector), intent(in)    :: a
+            real(kind = dp), intent(in) :: b
+            type(vector)                :: c
+
+            c%i = a%i * b
+            c%j = a%j * b
+            c%k = a%k * b
+        end function
+
+        pure function vec_mult_int_2(b, a) result(c)
+            type(vector), intent(in)    :: a
+            integer, intent(in)         :: b
+            type(vector)                :: c
+
+            real(kind = dp)             :: b_real
+
+            b_real = real(b, kind = dp)
+
+            c%i = a%i * b_real
+            c%j = a%j * b_real
+            c%k = a%k * b_real
+        end function
+
+        pure function vec_mult_real_2(b, a) result(c)
+            type(vector), intent(in)    :: a
+            real(kind = dp), intent(in) :: b
+            type(vector)                :: c
+
+            c%i = a%i * b
+            c%j = a%j * b
+            c%k = a%k * b
         end function
 
         pure function vec_div_int(a, b) result(c)
